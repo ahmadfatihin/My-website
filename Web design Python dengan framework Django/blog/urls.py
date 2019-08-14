b82f84d767cceee655a186768431147e51fc6787
ahmadfatihin <54095238+ahmadfatihin@users.noreply.github.com>
2019-08-14 13:58:27 +0700

    Add files via upload

---
+++ Web design Python dengan framework Django/blog/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from django.conf.urls import include
+
+from . import views
+
+urlpatterns = [
+	url(r'^$', views.index, name="index"),
+	url(r'^post/(?P<slugInput>[\w-]+)/$', views.detailPost, name="detail"),
+	url(r'^category/(?P<categoryInput>[\w-]+)/$', views.categoryPost, name="category"),
+]
---
+++ Web design Python dengan framework Django/mywebsite/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+
+from . import views
+
+urlpatterns = [
+	url(r'^$', views.index, name = "index"),
+	url(r'^blog/',include('blog.urls', namespace="blog")),
+	url(r'^tinymce/', include('tinymce.urls')),
+    url(r'^admin/',admin.site.urls),
+]
